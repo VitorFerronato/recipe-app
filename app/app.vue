4804e8863a8193e5f8c833132b0f611bf0c6c852
vitor eduardo <vitor.eduardo@predify.me>
2024-08-29 15:35:30 -0300

    feat: base navigation

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="font-Montserrat container">
-    <h1 class="text-lg text-blue-400">Hello world!</h1>
+  <div class="font-Montserrat ">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+
   </div>
 </template>
